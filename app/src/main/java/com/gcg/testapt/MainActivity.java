b009b0fccf2595bf927ddb7279153b7af200d537
gcg-git <876778019@qq.com>
2020-09-18 16:33:33 +0800

    添加activity生命周期日志打印

--- app/src/main/java/com/gcg/testapt/MainActivity.java
+++ app/src/main/java/com/gcg/testapt/MainActivity.java
@@ -3,6 +3,7 @@ package com.gcg.testapt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.gcg.my_an_lib.Inject;
 
@@ -13,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v("wocao", "MainActivity onCreate");
     }
 }
